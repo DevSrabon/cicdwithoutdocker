88dd46504f5b5b5de89e8e0543636b3c07448e70
SRABON BARUA <108211748+DevSrabon@users.noreply.github.com>
2024-04-23 01:35:08 +0600

    Update server.ts

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript with cicd Server jenkins texst 2");
+  res.send("Express + TypeScript with cicd Server jenkins texst d2");
 });
 
 app.listen(port, () => {
